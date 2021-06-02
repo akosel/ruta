2313b351571679112db2c94dabc9323cca3f54cb
Aaron Kosel <aaronjkosel@gmail.com>
2021-06-02 20:59:35 +0000

    Add file for bootstrapping dependencies

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+
+# most commands run with make
+sudo apt install make
+
+# new gpio library that works with newer linux kernels
+# excellent post on this here: https://waldorf.waveform.org.uk/2021/the-pins-they-are-a-changin.html
+sudo apt install python3-lgpio
+
+# this might be bad form, but make sure python is a valid command
+sudo apt install python-is-python3
+
+# install pip
+sudo apt install python3-pip
+
+# install poetry for python dependency management
+# see https://python-poetry.org/docs/ for details
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+
+make install
