1a13f22b1164b4744ee25fd37de459033a1c4b76
Aaron Kosel <aaron.kosel@zapier.com>
2021-06-03 17:47:51 -0400

    Add support for dry-run in job scheduler

--- freyr/irrigate/management/commands/run_scheduled_jobs.py
+++ freyr/irrigate/management/commands/run_scheduled_jobs.py
@@ -1,7 +1,15 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 from irrigate.schedule import run_all
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        run_all()
+    def add_arguments(self, parser):
+        parser.add_argument('--dry-run', action='store_true')
+
+    def handle(self, dry_run=False, *args, **kwargs):
+        logger.info(f'Checking for runnable jobs. dry_run: {dry_run}')
+        run_all(dry_run=dry_run)
+        logger.info(f'Finished checking for runnable jobs. dry_run: {dry_run}')
