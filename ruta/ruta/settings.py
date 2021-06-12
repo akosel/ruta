499faef70a4eeeea51eb6e578a3c017cf9eea9e5
Aaron Kosel <aaronjkosel@gmail.com>
2021-06-12 17:35:36 -0400

    Properly setup settings

--- ruta/ruta/settings.py
+++ ruta/ruta/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-olxd*&s8#5#)@0e^$prlawy#38=_5=a7xwg!xt&2czkp=f_3uw"
+SECRET_KEY = os.getenv('RUTA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
