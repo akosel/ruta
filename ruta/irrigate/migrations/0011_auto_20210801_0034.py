acdbc1225a8db3fa74021bb5fa40b6537fa7b343
Aaron Kosel <aaronjkosel@gmail.com>
2021-08-01 00:50:45 +0000

    Format migration file

--- ruta/irrigate/migrations/0011_auto_20210801_0034.py
+++ ruta/irrigate/migrations/0011_auto_20210801_0034.py
@@ -6,17 +6,19 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('irrigate', '0010_alter_actuatorrunlog_end_datetime'),
+        ("irrigate", "0010_alter_actuatorrunlog_end_datetime"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='actuatorrunlog',
-            options={'ordering': ('-start_datetime',)},
+            name="actuatorrunlog",
+            options={"ordering": ("-start_datetime",)},
         ),
         migrations.AddField(
-            model_name='scheduletime',
-            name='run_type',
-            field=models.IntegerField(choices=[(0, 'Recurring'), (1, 'One Off')], default=0),
+            model_name="scheduletime",
+            name="run_type",
+            field=models.IntegerField(
+                choices=[(0, "Recurring"), (1, "One Off")], default=0
+            ),
         ),
     ]
